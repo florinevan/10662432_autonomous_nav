acd65c2bae4b55a8cba03202d5d48bdd0463bd61
student <florine.vancappel@live.fr>
2020-02-18 15:24:55 +0000

    update source file

--- turtlebot2_wss/source_all.bash
+++ turtlebot2_wss/source_all.bash
@@ -1,14 +1,16 @@
 #! /bin/bash 
 
-TURTLEBOT_DRIVERS_WS=~/turtlebot2_wss/turtlebot
+TURTLEBOT_DRIVERS_WS=~/ROCO506/turtlebot2_wss/turtlebot
 source $TURTLEBOT_DRIVERS_WS/devel/setup.bash --extend
  
-TURTLEBOT_INTERACTION_WS=~/turtlebot2_wss/turtlebot_interaction
+TURTLEBOT_INTERACTION_WS=~/ROCO506/turtlebot2_wss/turtlebot_interaction
 source $TURTLEBOT_INTERACTION_WS/devel/setup.bash --extend
  
-TURTLEBOT_MSGS_WS=~/turtlebot2_wss/turtlebot_msgs
+TURTLEBOT_MSGS_WS=~/ROCO506/turtlebot2_wss/turtlebot_msgs
 source $TURTLEBOT_MSGS_WS/devel/setup.bash --extend
 
-TURTLEBOT_SIMULATION_WS=~/turtlebot2_wss/turtlebot_simulation
+TURTLEBOT_SIMULATION_WS=~/ROCO506/turtlebot2_wss/turtlebot_simulation
 source $TURTLEBOT_SIMULATION_WS/devel/setup.bash --extend
   
+TURTLEBOT_MAP_AND_NAV_WS=~/ROCO506/turtlebot2_wss/turtlebot_map_and_nav
+source $TURTLEBOT_MAP_AND_NAV_WS/devel/setup.bash ­­--extend
